fbb14dd31f4a4c804b91fa4f1cab97766e8c421f
kipariss1 <kpassudin@gmail.com>
2024-03-07 00:10:53 +0100

    continue with tutorial

---
+++ gym-tracker-web/src/pages/MemoriesPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>All memories</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <h2>These are my memories!</h2>
+            <ion-list>
+                <ion-item>Ivan</ion-item>
+                <ion-item>Alexandr</ion-item>
+                <ion-item>Gorshok</ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+    import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/vue';
+
+    export default {
+        components: {
+            IonPage: IonPage,
+            IonHeader: IonHeader,
+            IonToolbar: IonToolbar,
+            IonTitle: IonTitle,
+            IonContent: IonContent,
+            IonList: IonList,
+            IonItem: IonItem,
+        }
+    }
+</script>
